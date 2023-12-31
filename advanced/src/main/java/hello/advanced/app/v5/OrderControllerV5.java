c733e702ac05f3463ad1d7d31e766623a32e87b3
ksh <tjdgus2319@gmail.com>
2023-12-31 16:48:51 +0900

    데코레이션 패턴 로그추적기 v1 버전에 적용

---
+++ advanced/src/main/java/hello/advanced/app/v5/OrderControllerV5.java
@@ -0,0 +1,26 @@
+package hello.advanced.app.v5;
+
+import hello.advanced.trace.callback.TraceCallback;
+import hello.advanced.trace.callback.TraceTemplate;
+import hello.advanced.trace.logtrace.LogTrace;
+import hello.advanced.trace.template.AbstractTemplate;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class OrderControllerV5 {
+
+    private final OrderServiceV5 orderService;
+    private final TraceTemplate template;
+
+    @GetMapping("/v5/request")
+    public String request(String itemId) {
+        return template.execute("OrderController.request()", () -> {
+            orderService.orderItem(itemId);
+            return "ok";
+        });
+    }
+
+}
---
+++ proxy/src/main/java/hello/proxy/ProxyApplication.java
@@ -0,0 +1,26 @@
+package hello.proxy;
+
+import hello.proxy.Config.AppV1Config;
+import hello.proxy.Config.AppV2Config;
+import hello.proxy.Config.v1_proxy.InterfaceProxyConfig;
+import hello.proxy.trace.logtrace.LogTrace;
+import hello.proxy.trace.logtrace.ThreadLocalLogTrace;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
+
+@SpringBootApplication(scanBasePackages = "hello.proxy.app.v3") //주의
+//@Import({AppV2Config.class, AppV1Config.class})
+@Import(InterfaceProxyConfig.class)
+public class ProxyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProxyApplication.class, args);
+	}
+
+	@Bean
+	public LogTrace logTrace() {
+		return new ThreadLocalLogTrace();
+	}
+}
