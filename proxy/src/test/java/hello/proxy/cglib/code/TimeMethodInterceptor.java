caf293173b58d8ed891f6f4307d091f0e8aa7cd6
ksh <tjdgus2319@gmail.com>
2023-12-31 20:42:14 +0900

    CGLIB 테스트

--- proxy/src/test/java/hello/proxy/cglib/code/TimeMethodInterceptor.java
+++ proxy/src/test/java/hello/proxy/cglib/code/TimeMethodInterceptor.java
@@ -1,21 +1,22 @@
-package hello.proxy.jdkdynamic.code;
+package hello.proxy.cglib.code;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.cglib.proxy.MethodInterceptor;
+import org.springframework.cglib.proxy.MethodProxy;
 
-import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 
 @Slf4j
-public class TimeInvocationHandler implements InvocationHandler {
+public class TimeMethodInterceptor implements MethodInterceptor {
 
-    private final Object target;
+    private Object target;
 
-    public TimeInvocationHandler(Object target) {
+    public TimeMethodInterceptor(Object target) {
         this.target = target;
     }
 
     @Override
-    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+    public Object intercept(Object obj, Method method, Object[] args, MethodProxy proxy) throws Throwable {
         log.info("TimeProxy 실행");
         long startTime = System.currentTimeMillis();
 
@@ -24,6 +25,6 @@ public class TimeInvocationHandler implements InvocationHandler {
         long endTime = System.currentTimeMillis();
         long resultTime = endTime - startTime;
         log.info("timePrxoy 종료 resultTime = {}",resultTime);
-        return resultTime;
+        return result;
     }
 }
